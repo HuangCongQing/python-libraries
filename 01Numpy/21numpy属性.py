46b994e612f1fb3e2d6abdb69d05cfde1b8ff90e
阿小庆 <1756260160@qq.com>
2019-03-07 22:59:54 +0800

    numpy属性

---
+++ 01Numpy/21numpy属性.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array1 = np.array([[1,2,3],
+ [3,4,5]]) # 列表转化为矩阵
+
+print(array1)
+print('几维number of dim:', array1.ndim)
+print('shape:', array1.shape)
+print('size:', array1.shape)
+
+
