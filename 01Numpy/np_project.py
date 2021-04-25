3334d7535818360bb942aa287860a46355708905
HuangCongQing <1756260160@qq.com>
2021-04-25 19:57:02 +0800

    add: 项目需要

---
+++ 01Numpy/np_project.py
@@ -0,0 +1,19 @@
+'''
+Description: numpy项目相关
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-04-25 19:49:38
+LastEditTime: 2021-04-25 19:56:47
+FilePath: /python-libraries/01Numpy/np_project.py
+'''
+import numpy as np
+
+# A = [[ 3  4  5  6]
+#  [ 7  8  9 10]
+#  [11 12 13 14]]
+A = np.arange(3,15).reshape((3,4))
+
+index = np.arange(1,2) # [1]
+print(index)
+print(A[index])  # [[ 7  8  9 10]]
